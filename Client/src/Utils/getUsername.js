91242bc91e8cdc204efe1b41b3d4630deb271bda
Gyaansh <gyaansh70@gmail.com>
2026-04-26 18:08:32 +0530

    fix: authentication

--- Client/src/Utils/getUsername.js
+++ Client/src/Utils/getUsername.js
@@ -1,15 +1,21 @@
 async function getUsername() {
   try {
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/user/profile/username`);
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/user/profile/username`,
+      {
+        credentials: "include" 
+      }
+    );
+
     const userData = await res.json();
-    if(res.ok){
+
+    if (res.ok) {
       return userData.data.username;
-    }
-    else{
-      return userData.message
+    } else {
+      return null; // better than returning message
     }
   } catch (err) {
     console.log(err);
+    return null;
   }
 }
-export default getUsername;
